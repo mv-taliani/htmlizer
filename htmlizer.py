0cb15c01e843ece56ab9e336ffdf22a367b4e1af
mv_taliani <mv.taliani@gmail.com>
2023-09-27 12:57:32 -0300

    Decorator factory para gerar css

--- htmlizer.py
+++ htmlizer.py
@@ -7,14 +7,30 @@ class Tag:
     name: str
     attributes: dict = field(default_factory=dict)
     content: str | None = ''
-    selector_name: str = ''
+    styles: dict = field(default_factory=dict)
+    # selector_name: str = ''
 
     @property
     def html(self):
         attributes = " ".join(f'{key}="{value}"' for key, value in self.attributes.items())
+        style = ''
+        if self.styles:
+            style = 'style= "' + "; ".join(f'{css_prop}: {css_value}' for css_prop, css_value in self.styles.items()
+                                           ) + '"'
         if self.content:
-            return f'<{self.name} {attributes}> {self.content} </{self.name}>'
-        return f'<{self.name} {attributes}/>'
+            return f'<{self.name} {attributes} {style}> {self.content} </{self.name}>'
+        return f'<{self.name} {style} {attributes}/>'
+
+
+def create_css(**styles):
+    def wrapped(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            result: Tag = func(*args, **kwargs)
+            result.styles.update(styles)
+            return result
+        return inner
+    return wrapped
 
 
 def htmlize(name, **props):
@@ -22,13 +38,13 @@ def htmlize(name, **props):
         @wraps(func)
         def inner(*args, **kwargs):
             result = func(*args, *kwargs)
-            return Tag(name=name, attributes=props, content=result)
+            try:
+                content = result.html
+            except AttributeError:
+                content = result
+
+            return Tag(name=name, attributes=props, content=content)
 
         return inner
 
     return wrapped
-
-
-def csslize(prop: str, **styles):
-    css = (f'  {proper}: "{value}"' for proper, value in styles.items())
-    return prop + " { \n" + ";\n".join(css) + "\n}"
